6d43ba029d6810009e6458eca8d877de04a9a5d8
azhunusbekov <azhunusbekov@one.kz>
2024-05-31 14:26:06 +0500

    node js deploy

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -1,6 +1,6 @@
 const ErrorHandler = (err, req, res, next) => {
   const errStatus = err.statusCode || 500;
-  const errMsg = err.message || 'Something went wrong';
+  const errMsg = err.message || 'Something went wrong!';
   res.status(errStatus).json({
     success: false,
     status: errStatus,
